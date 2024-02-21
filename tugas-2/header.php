85444e746209e04b35d75535a3c03a0836c0f39b
Your Name <you@example.com>
2024-02-21 08:37:09 +0700

    first commit

---
+++ tugas-2/header.php
@@ -0,0 +1,38 @@
+<div class="wrapper row0">
+  <div id="topbar" class="clear"> 
+    <!-- ################################################################################################ -->
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">A - Z Index</a></li>
+        <li><a href="#">Student Login</a></li>
+        <li><a href="#">Staff Login</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ --> 
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <img src="../tugas-2/images/demo/logosmk.png" alt="">
+      <h1><a href="index.html">SMKN 64 JAKARTA</a></h1>
+      <p>Sukses(Smart,unggul,kreatif,semangat,empati,dan sopan)</p>
+    </div>
+    <div class="fl_right">
+      <form class="clear" method="post" action="#">
+        <fieldset>
+          <legend>Search:</legend>
+          <input type="text" value="" placeholder="Search Here">
+          <button class="fa fa-search" type="submit" title="Search"><em>Search</em></button>
+        </fieldset>
+      </form>
+    </div>
+    <!-- ################################################################################################ --> 
+  </header>
+</div>
---
+++ tugas-2/index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<!--
+Template Name: Academic Education V2
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>SMKN 64 JAKARTA</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<link rel="icon" href="../tugas-2/images/demo/logosmk.png">
+</head>
+<body id="top">
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<?php include "header.php"; ?>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row2">
+  <div class="rounded">
+    <nav id="mainav" class="clear"> 
+      <!-- ################################################################################################ -->
+      <ul class="clear">
+        <li class="active"><a href="index.html">beranda</a></li>
+        <li class="clear"><a href="pages/sidebar-left.html">Visi Misi</a></li>
+        <li class="clear"><a href="pages/sidebar-left-2.html">Profil</a></li>
+        <li><a href="#">informasi sekolah</a></li>
+        <li><a href="#">guru dan tu</a></li>
+        <li><a class="drop" href="#">E-perpustakaan</a>
+          <ul>
+            <li><a href="pages/gallery.html">buku pelajaran</a></li>
+            <li><a href="pages/portfolio.html">buku fiksi</a></li>
+            <li><a href="pages/full-width.html">cerpen</a></li>
+            <li><a href="pages/sidebar-left.html">cerita rakyat</a></li>
+            <li><a href="pages/sidebar-left-2.html">pojok kisah</a></li>
+            <li><a href="pages/sidebar-right.html">tepian sajak</a></li>
+          </ul>
+        </li>
+        <li><a class="drop" href="#">kategori</a>
+          <ul>
+            <li><a href="#">pkl</a></li>
+            <li><a href="#">kopi 64</a></li>
+            
+          </ul>
+        </li>
+        <li><a class="drop" href="#">direktori</a>
+          <ul>
+            <li><a href="#">direktori alumni</a></li>
+            <li><a href="#">pendaftaran alumni</a></li>
+            <li><a href="#">direktori peserta didik</a></li>
+          </ul>
+        </li>
+        <li><a class="drop" href="#">galeri</a>
+          <ul>
+            <li><a href="./pages/gallery.html">galeri foto</a></li>
+            <li><a href="#">galeri video</a></li>
+          </ul>
+        </li>
+        <li><a href="#">hubungi kami</a></li>
+        
+      </ul>
+      <!-- ################################################################################################ --> 
+    </nav>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper">
+  <div id="slider">
+    <div id="slide-wrapper" class="rounded clear"> 
+      <!-- ################################################################################################ -->
+      <figure id="slide-1"><a class="view" href="#"><img src="images/demo/slider/1.png" alt=""></a>
+        <figcaption>
+          <h2>Nullamlacus dui ipsum</h2>
+          <p>Attincidunt vel nam a maurisus lacinia consectetus magnisl sed ac morbi. Inmaurisus habitur pretium eu et ac vest penatibus id lacus parturpis.</p>
+          <p class="right"><a href="#">Continue Reading &raquo;</a></p>
+        </figcaption>
+      </figure>
+      <figure id="slide-2"><a class="view" href="#"><img src="images/demo/slider/2.png" alt=""></a>
+        <figcaption>
+          <h2>Aliquatjusto quisque nam</h2>
+          <p>Attincidunt vel nam a maurisus lacinia consectetus magnisl sed ac morbi. Inmaurisus habitur pretium eu et ac vest penatibus id lacus parturpis.</p>
+          <p class="right"><a href="#">Continue Reading &raquo;</a></p>
+        </figcaption>
+      </figure>
+      <figure id="slide-3"><a class="view" href="#"><img src="images/demo/slider/3.png" alt=""></a>
+        <figcaption>
+          <h2>Aliquatjusto quisque nam</h2>
+          <p>Attincidunt vel nam a maurisus lacinia consectetus magnisl sed ac morbi. Inmaurisus habitur pretium eu et ac vest penatibus id lacus parturpis.</p>
+          <p class="right"><a href="#">Continue Reading &raquo;</a></p>
+        </figcaption>
+      </figure>
+      <figure id="slide-4"><a class="view" href="#"><img src="images/demo/slider/4.png" alt=""></a>
+        <figcaption>
+          <h2>Aliquatjusto quisque nam</h2>
+          <p>Attincidunt vel nam a maurisus lacinia consectetus magnisl sed ac morbi. Inmaurisus habitur pretium eu et ac vest penatibus id lacus parturpis.</p>
+          <p class="right"><a href="#">Continue Reading &raquo;</a></p>
+        </figcaption>
+      </figure>
+      <figure id="slide-5"><a class="view" href="#"><img src="images/demo/slider/5.png" alt=""></a>
+        <figcaption>
+          <h2>Dapiensociis temper donec</h2>
+          <p>Attincidunt vel nam a maurisus lacinia consectetus magnisl sed ac morbi. Inmaurisus habitur pretium eu et ac vest penatibus id lacus parturpis.</p>
+          <p class="right"><a href="#">Continue Reading &raquo;</a></p>
+        </figcaption>
+      </figure>
+      <!-- ################################################################################################ -->
+      <ul id="slide-tabs">
+        <li><a href="#slide-1">turnamen basket</a></li>
+        <li><a href="#slide-2">gambar bentuk sekolah</a></li>
+        <li><a href="#slide-3">kumpul tu</a></li>
+        <li><a href="#slide-4">semua guru</a></li>
+        <li><a href="#slide-5">turnamen futsal</li>
+      </ul>
+      <!-- ################################################################################################ --> 
+    </div>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <div class="rounded">
+    <main class="container clear"> 
+      <!-- main body --> 
+      <!-- ################################################################################################ -->
+      <div class="group btmspace-30"> 
+        <!-- Left Column -->
+        <div class="one_quarter first"> 
+          <!-- ################################################################################################ -->
+          <ul class="nospace">
+            <li class="btmspace-15"><a href="#"><em class="heading">Pengumuman Hasil Tes Mutasi Masuk Semester Ganjil TP 2023_2024 Gel-1</em> <img class="borderedbox" src="images/demo/pengumuman.png" alt=""></a></li>
+            <li class="btmspace-15"><a href="#"><em class="heading">ujian semester ganjil</em> <img class="borderedbox" src="images/demo/ujian semester ganjil.png" alt=""></a></li>
+            <li class="btmspace-15"><a href="#"><em class="heading">sidang pkl</em> <img class="borderedbox" src="images/demo/sidang pkl.png" alt=""></a></li>
+            <li><a href="#"><em class="heading">pengumuman mutasi</em> <img class="borderedbox" src="images/demo/pengumuman mutasi.png" alt=""></a></li>
+          </ul>
+          <!-- ################################################################################################ --> 
+        </div>
+        <!-- / Left Column --> 
+        <!-- Middle Column -->
+        <div class="one_half"> 
+          <!-- ################################################################################################ -->
+          <h2>Latest News &amp; Events</h2>
+          <ul class="nospace listing">
+            <li class="clear">
+              <div class="imgl borderedbox"><img src="images/demo/pemanfaatan limbah.png" alt=""></div>
+              <p class="nospace btmspace-15"><a href="#">pemanfaatan limbah bekas.</a></p>
+              <p>Membuat wadah dan tempat cuci tangan serta sanitizer saat ptm</a></p>
+            </li>
+            <li class="clear">
+              <div class="imgl borderedbox"><img src="images/demo/pelepasan kepsek.png" alt=""></div>
+              <p class="nospace btmspace-15"><a href="#">pelepasan kepala sekolah.</a></p>
+              <p>pelepasan kepala sekolah dan sambutan kepala sekolah.</p>
+            </li>
+            <li class="clear">
+              <div class="imgl borderedbox"><img src="images/demo/literasi.png" alt=""></div>
+              <p class="nospace btmspace-15"><a href="#">kegiatan literasi.</a></p>
+              <p>Kegiatan Literasi SMKN 64 jakarta</p>.
+            </li>
+          </ul>
+          <p class="right"><a href="#">Click here to view all of the latest news and events &raquo;</a></p>
+          <!-- ################################################################################################ --> 
+        </div>
+        <!-- / Middle Column --> 
+        <!-- Right Column -->
+        <div class="one_quarter sidebar"> 
+          <!-- ################################################################################################ -->
+          <div class="sdb_holder">
+            <h6>DEWI PUSPITASARI, S.ST.PAR, M.PAR</h6>
+            <div class="mediacontainer"><img src="images/demo/kepsek.png" alt="">
+              <p><a href="#"> Kepala Sekolah-
+              </br>
+                Sebagai lembaga pendidikan, SMKN 64 Jakarta tanggap dengan perkembangan teknologi tersebut. Dengan dukungan SDM yang di miliki sekolah ini siap…</a></p>
+            </div>
+          </div>
+          <div class="sdb_holder">
+            <h6>Quick Information</h6>
+            <ul class="nospace quickinfo">
+              <li class="clear"><a href="#"><img src="../tugas-2/images/demo/yt1.jpg" alt=""> video youtube 1</a></li>
+              <li class="clear"><a href="#"><img src="../tugas-2/images/demo/yt2.jpg" alt=""> video youtube 2</a></li>
+            </ul>
+          </div>
+          <!-- ################################################################################################ --> 
+        </div>
+        <!-- / Right Column --> 
+      </div>
+      <!-- ################################################################################################ --> 
+      <!-- ################################################################################################ -->
+      <div id="twitter" class="group btmspace-30">
+        <div class="one_quarter first center"><a href="https://www.instagram.com/smkn64jakarta/"><i class="fa fa-instagram fa-3x"></i><br>
+          follow instagram kami</a></div>
+        <div class="three_quarter bold">
+          <p>untuk informasi dan karya siswa siswi smkn 64 jakarta <p>
+        </div>
+      </div>
+      <!-- ################################################################################################ --> 
+      <!-- ################################################################################################ -->
+      <div class="group">
+        <h2>Quickly Find What You Are Looking For</h2>
+        <div class="one_quarter first"> 
+          <!-- ################################################################################################ -->
+          <ul class="nospace">
+            <li><a href="#">smkn 64 jakarta</a></li>
+            <li><a href="#">LKS 2023</a></li>
+            <li><a href="#">Kopi 64</a></li>
+            
+          </ul>
+          <!-- ################################################################################################ --> 
+        </div>
+        <div class="one_quarter"> 
+          <!-- ################################################################################################ -->
+          <ul class="nospace">
+            <li><a href="#">smk 64 hebat</a></li>
+            <li><a href="#">smk 64 bisa</a></li>
+            <li><a href="#">smk 64 unggul</a></li>
+           
+          </ul>
+          <!-- ################################################################################################ --> 
+        </div>
+        <div class="one_quarter"> 
+          <!-- ################################################################################################ -->
+          <ul class="nospace">
+            <li><a href="#">numerasi</a></li>
+            <li><a href="#">smk 64 jaya</a></li>
+            <li><a href="#">smk 64 aman</a></li>
+           
+          </ul>
+          <!-- ################################################################################################ --> 
+        </div>
+        <div class="one_quarter"> 
+          <!-- ################################################################################################ -->
+          <ul class="nospace">
+            <li><a href="#">Office of the Registrar</a></li>
+            <li><a href="#">Online Learning</a></li>
+            <li><a href="#">Parent Information</a></li>
+            
+          </ul>          
+
+          <!-- ################################################################################################ --> 
+        </div>
+      </div>
+      <!-- ################################################################################################ --> 
+      <!-- / main body -->
+      <div class="clear"></div>
+    </main>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <div class="rounded">
+    <footer id="footer" class="clear"> 
+      <!-- ################################################################################################ -->
+      <div class="one_third first">
+        <figure class="center"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.106411678014!2d106.86680617395389!3d-6.249706893738723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f34a18a336c7%3A0x3a85f62d85e1ba8a!2sSMK%20Negeri%2064%20Jakarta%20Timur!5e0!3m2!1sid!2sid!4v1696211446537!5m2!1sid!2sid" width="250" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <figcaption><a href="https://maps.app.goo.gl/ivJyUgiPeBCNAaX98">Find Us With Google Maps &raquo;</a></figcaption>
+        </figure>
+      </div>
+      <div class="one_third">
+        <address>
+          Gifted School :, Jl. Jaani Nasir<br>
+         RT.5/RW.11, Cawang<br>
+         Kec. Kramat jati<br>
+        Daerah Khusus Ibukota Jakarta 13630<br>
+        <br>
+        <i class="fa fa-phone pright-10"> 08978891895 (hilal)</i><br>
+        <i class="fa fa-phone pright-10"> 081212001521 (agus)</i><br>
+        <i class="fa fa-envelope-o pright-10"></i> <a href="#">info@smkn64-jkt.sch.id</a>
+        </address>
+      </div>
+      <div class="one_third">
+        <p class="nospace btmspace-10">Stay Up to Date With What's Happening</p>
+        <ul class="faico clear">
+          <li><a class="faicon-twitter" href="https://www.instagram.com/smkn64jakarta/"><i class="fa fa-instagram"></i></a></li>
+          <li><a class="faicon-linkedin" href="https://www.youtube.com/@smkn64jakarta22"><i class="fa fa-youtube"></i></a></li>
+        </ul>
+        <form class="clear" method="post" action="#">
+          <fieldset>
+            <legend>Subscribe To Our Newsletter:</legend>
+            <input type="text" value="" placeholder="Enter Email Here&hellip;">
+            <button class="fa fa-sign-in" type="submit" title="Sign Up"><em>Sign Up</em></button>
+          </fieldset>
+        </form>
+      </div>
+      <!-- ################################################################################################ --> 
+    </footer>
+  </div>
+</div>
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ --> 
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2014 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ --> 
+  </div>
+</div>
+<!-- JAVASCRIPTS --> 
+<script src="layout/scripts/jquery.min.js"></script> 
+<script src="layout/scripts/jquery.fitvids.min.js"></script> 
+<script src="layout/scripts/jquery.mobilemenu.js"></script> 
+<script src="layout/scripts/tabslet/jquery.tabslet.min.js"></script>
+</body>
+</html>
